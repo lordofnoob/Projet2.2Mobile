24dc203a3d868471140f6d99fcd2c1c9d78ce1b3
Timothé <tdauplay97@gmail.com>
2018-10-26 16:13:17 +0200

    Ajout Android, FPS Display, Enlevage de lumières , rearrengement des folders

---
+++ Assets/Scripts/FPSDisplay.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSDisplay : MonoBehaviour
+{
+
+    //Calcul des FPS
+
+    public float updateInterval = 0.5f;
+    public float accum = 0f;
+    public float frames = 0f;
+    public float timeleft;
+
+    void Start()
+    {
+        timeleft = updateInterval;
+    }
+
+    void Update()
+    {
+        timeleft -= Time.deltaTime;
+        accum += Time.timeScale / Time.deltaTime;
+        ++frames;
+
+        if (timeleft <= 0.0)
+        {
+            if (accum / frames < 30)
+            {
+                transform.GetComponent<Text>().color = Color.red;
+            }
+
+            if (accum / frames > 30 && accum / frames < 60)
+            {
+                transform.GetComponent<Text>().color = Color.yellow;
+            }
+
+            if (accum / frames > 60)
+            {
+                transform.GetComponent<Text>().color = Color.cyan;
+            }
+
+            transform.GetComponent<Text>().text = (accum / frames).ToString("f1"); // f1 -> tronque la valeur
+            timeleft = updateInterval;
+            accum = 0.0f;
+            frames = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+        // Débloquer les 60 FPS pour le télephone
+        private void start()
+        {
+            Application.targetFrameRate = 60;
+        }
+}

33dfeabe626fc318ec780fe47357df9c9d1a7b77
Timothé <tdauplay97@gmail.com>
2018-11-13 17:19:25 +0100

    AddScrolling

---
+++ Assets/Scripts/ScrollingManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingManager : MonoBehaviour
+{
+    [Header("Target")]
+    public GameObject Player;
+
+    [Range(1, 100), SerializeField, Header("Speed")]
+    private float movementSpeed = 1;
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * movementSpeed); //Deplacement de la zone vers la droite.
+    }
+
+    private void OnTriggerEnter(Collider col) //On regarde si le joueur entre dans la zone
+    {
+        if(col.gameObject == Player)
+        {
+            Debug.Log("Player Dead");
+        }
+    }
+}
